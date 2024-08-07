220dcc2e09cf558b604aa7d18ed9086bbb3d99e1
Alex.Koh <alexkzw@hotmail.com>
2024-08-07 16:55:41 +1200

    reorder tests to check that X and y are in the correct format before testing for NA and infinite values

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
 #helper function to check for errors
 check_for_errors <- function(X, y) {
+    if (!is.matrix(X)) stop("X should be a matrix")
+    if (!is.numeric(y)) stop("y should be a numeric vector")
+    if (nrow(X) != length(y)) stop("Dimensions of X and y do not match")
     if (any(is.na(X))) stop("NA values in X")
     if (any(is.na(y))) stop("NA values in y")
     if (any(is.infinite(X))) stop("Inf values in X")
     if (any(is.infinite(y))) stop("Inf values in y")
-    if (!is.matrix(X)) stop("X should be a matrix")
-    if (!is.numeric(y)) stop("y should be a numeric vector")
-    if (nrow(X) != length(y)) stop("Dimensions of X and y do not match")
 }
